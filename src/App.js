0a5f650dac9171738094ba97e1a0ca28d8d2a518
Roy van de Water <roy.v.water@gmail.com>
2024-10-27 10:47:22 -0700

    Implement the core logic

--- src/App.js
+++ src/App.js
@@ -5,16 +5,59 @@ import { useEffect } from "preact/hooks";
 import { fetchRandomBird } from "./fetchRandomBird.js";
 
 export const App = () => {
-  const data = useSignal(1);
+  const data = useSignal();
+  const answer = useSignal();
 
   useEffect(async () => {
     data.value = await fetchRandomBird();
   }, []);
 
+  const onAnswerReal = () => {
+    answer.value = "real";
+  };
+
+  const onAnswerFake = () => {
+    answer.value = "fake";
+  };
+
   if (!data.value) return null;
 
+  return html`<div class="App">
+    <div class="Content">
+      <h1 class="BirdName">${data.value.bird}</h1>
+      <img
+        src="https://cdn.royvandewater.com/birdnotbird/${data.value.filename}"
+      />
+
+      ${answer.value
+        ? html`<${Result} answer=${answer} data=${data} />`
+        : html`<${Chooser}
+            onAnswerReal=${onAnswerReal}
+            onAnswerFake=${onAnswerFake}
+          /> `}
+    </div>
+  </div>`;
+};
+
+const Chooser = ({ onAnswerReal, onAnswerFake }) => {
   return html`<div>
-    <h1>${data.value.bird}</h1>
-    <p>${data.value.isReal ? "Real" : "Fake"}</p>
+    <button className="Real Choice" onClick=${onAnswerReal}>Real</button>
+    <button className="Fake Choice" onClick=${onAnswerFake}>Fake</button>
+  </div>`;
+};
+
+const Result = ({ answer, data }) => {
+  const correctAnswer = data.value.isReal ? "real" : "fake";
+
+  if (answer.value === correctAnswer) {
+    return html`<div>
+      <h1>Correct!</h1>
+      <p>This bird is <strong>${correctAnswer}</strong>!</p>
+    </div>`;
+  }
+
+  return html`<div>
+    <h1>Wrong!</h1>
+    <p>This bird is actually <strong>${correctAnswer}</strong>!</p>
   </div>`;
 };
--- src/fetchRandomBird.js
+++ src/fetchRandomBird.js
@@ -1,14 +1,35 @@
+import { capitalize, first, join, map } from "remeda";
+import { pipe, piped, sample, split, toLowerCase } from "remeda";
 import { fetchRealBirds, fetchFakeBirds } from "./fetchBirds.js";
 
+const replaceAll = (pattern, replacement) => (str) => {
+  return str.replaceAll(pattern, replacement);
+};
+
+const replace = (pattern, replacement) => (str) => {
+  return str.replace(pattern, replacement);
+};
+
+const sample1 = piped(sample(1), first);
+const capitalizeWords = piped(split(" "), map(capitalize()), join(" "));
+
+const toFilename = piped(
+  replaceAll(" ", "-"),
+  replaceAll("'", ""),
+  toLowerCase,
+  replace(/$/, ".jpg")
+);
+
 export const fetchRandomBird = async () => {
-  const real = Math.random() < 0.5;
+  const isReal = Math.random() < 0.5;
 
-  const birds = real ? await fetchRealBirds() : await fetchFakeBirds();
-  const bird = birds[Math.floor(Math.random() * birds.length)];
-  const filename = bird
-    .replaceAll(" ", "-")
-    .replaceAll("'", "") // some birds have posessive names
-    .toLowerCase();
+  const birds = isReal ? await fetchRealBirds() : await fetchFakeBirds();
 
-  return { bird, isReal: real, filename };
+  const bird = pipe(birds, sample1, capitalizeWords);
+
+  const filename = toFilename(bird);
+
+  console.log(bird, isReal, filename);
+
+  return { bird, isReal, filename };
 };
